615bd70a61a7a33aac82ca477a628b1af7088c41
aCkoo420 <47101303+aCkoo420@users.noreply.github.com>
2021-12-31 19:24:01 +0100

    Add files via upload

---
+++ Mob.cs
@@ -0,0 +1,31 @@
+/*using Godot;
+using System;
+
+public class Mob : KinematicBody2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var animSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		animSprite2D.Playing = true;
+		string[] mobTypes = animSprite2D.Frames.GetAnimationNames();
+		animSprite2D.Animation = mobTypes[GD.Randi() % mobTypes.Length];
+	}
+	
+	public void OnVisibilityNotifier2DScreenExited()
+	{
+		QueueFree();
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
+*/
---
+++ Svet2.cs
@@ -0,0 +1,60 @@
+using Godot;
+using System;
+
+public class Svet2 : Node2D
+{
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+
+	// Called when the node enters the scene tree for the first time.
+
+   
+
+	public override void _Ready()
+	{
+	
+	}
+		private void _on_OnePlayer_pressed()
+	{
+		GetNode<KinematicBody2D>("Penguwin2").Hide();
+		GetNode<Button>("OnePlayer").Hide();
+		
+	}
+
+	private void _on_Button_pressed()
+	{
+		GetTree().ChangeScene("res://TileMap.tscn");
+	}
+	
+		
+		private void _on_Continue_pressed()
+	{
+		GetTree().Paused = false;
+		GetNode<Button>("Quit").Hide();
+		GetNode<Button>("Continue").Hide();
+	}
+  
+ /* public override void _Process(float delta)
+  {
+	 
+ 	}*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
